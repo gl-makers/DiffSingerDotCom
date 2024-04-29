d172215db7c368550337bdaa1ccfea7e54e17930
HuanLinOTO <linsmc@126.com>
2024-04-29 11:07:13 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,18 +4,9 @@
     <template v-for="item in processed_models">
       <Link
         :model="item"
-        :style="{
-          left: full_screen_model_id === item.id ? '100px' : pos[item.id].x + 'px',
-          top: full_screen_model_id === item.id ? `${Number(scrolledY)+120}px` : pos[item.id].y + 'px',
-          width: full_screen_model_id === item.id ? `calc(100vw - 200px)` : widths[item.id] + 'px',
-          opacity: visible_models_id.indexOf(item.id) === -1 ? 0 : 1,
-          'z-index': full_screen_model_id === item.id ? 99999999 : 0,
-          height: full_screen_model_id === item.id ? `calc(100vh - 200px)` : undefined,
-          position: full_screen_model_id === item.id ? 'absolute' : 'absolute',
-          transition: full_screen_model_id === item.id ? 'all 0.5s cubic-bezier(0.16, 0.02, 0, 0.99)' : 'all 0.3s ease-in-out',
-        }"
+        :style="styles[item.id]"
         ref="model_refs"
-        :is_full_screen="full_screen_model_id === item.id"
+        :is_full_screen="false"
         @click="openLink(item.link)"
         ></Link>
         <!-- @click="() => full_screen_model_id == item.id? full_screen_model_id = -1  : full_screen_model_id = item.id" -->
@@ -27,7 +18,7 @@
 </template>
 
 <script lang="ts" setup>
-import { VNodeRef, ref, watch } from 'vue';
+import { VNodeRef, onMounted, ref, watch } from 'vue';
 
 import Link from '../components/Link.vue';
 import links from '../links.json'
@@ -40,12 +31,6 @@ const openLink = (link: string) => {
 
 const col_num = ref(0)
 
-const processed_models = ref<(LinkType & { id: number })[]>([])
-const visible_models_id = ref<Number[]>([])
-
-const pos = ref<Record<number,{ x: number, y: number }>>({})
-const widths = ref<Record<number, number>>({})
-
 const gap = 20
 const padding_left = 100
 const padding_right = 100
@@ -53,6 +38,11 @@ const padding_top = 128
 
 const full_screen_model_id = ref(-1)
 const model_refs = ref<Record<number, VNodeRef>>({})
+
+const visible_models_id = ref<Record<number, boolean>>({})
+const processed_models = ref<(LinkType & { id: number })[]>([])
+
+const styles = ref<Record<number, any>>({})
 
 watch(full_screen_model_id, (val) => {
   if (val !== -1) {
@@ -62,8 +52,6 @@ watch(full_screen_model_id, (val) => {
     document.body.style.overflow = "auto"
   }
 })
-// @ts-ignore
-let observer: IntersectionObserver | null = null;
 
 // @ts-ignore
 function debounce(fn: any, delay=100) {
@@ -79,20 +67,65 @@ function debounce(fn: any, delay=100) {
 
   return _debounce
 }
-for (const i in links) {
-  // @ts-ignore
-  processed_models.value.push({
-    ...links[i],
-  // @ts-ignore
-    id: Number(i)
-  })
-  visible_models_id.value.push(Number(i))
+
+function init() {
+  for (const i in links) {
+    // @ts-ignore
+    processed_models.value.push({
+      ...links[i],
+    // @ts-ignore
+      id: Number(i)
+    })
+    visible_models_id.value[Number(i)] = true
+  }
 }
 
-const scrolledY = ref("0")
+const calcCardStyle = () => {
+  console.log("visible_models_id",visible_models_id.value);
+  const width = window.innerWidth
+  const card_width = (width - padding_left - padding_right - (col_num.value - 1) * gap) / col_num.value
+  const card_height = card_width * 9 / 16
 
-window.onscroll = () => {
-  scrolledY.value = window.scrollY.toFixed()
+  var cur_col = 0
+  var cur_row = 0
+  var animate_card_num = 0
+
+  for (const i in processed_models.value) {
+    if (!visible_models_id.value[i]) {
+      if (animate_card_num > 3) {
+        // 隐藏
+        styles.value[processed_models.value[i].id] = {
+          display: "none"
+        }
+      } else {
+        // 隐藏并移动到中央
+        styles.value[processed_models.value[i].id] = {
+          opacity: 0,
+          top: `56px`,
+          left: `${window.innerWidth / 2 - card_width / 2}px`,
+        }
+        animate_card_num ++;
+      }
+      continue
+    }
+    const model = processed_models.value[i]
+    const x = padding_left + cur_col * (card_width + gap)
+    const y = padding_top + cur_row * (card_height + gap)
+    styles.value[model.id] = {
+      width: `${card_width}px`,
+      height: `${card_height}px`,
+      top: `${y}px`,
+      left: `${x}px`,
+      transition: "all 0.5s",
+    }
+    if (cur_col === col_num.value - 1) {
+      cur_col = 0
+      cur_row += 1
+    } else {
+      cur_col += 1
+    }
+  }
+  console.log("calcCardStyle", styles);
 }
 
 // 根据窗口宽度计算列数
@@ -109,49 +142,34 @@ const calcColNum = () => {
   }
   console.log("col_num", col_num.value);
 
-  // 给每个 model 计算位置
-  const col_width = (width - padding_left - gap * (col_num.value - 1) - padding_right) / col_num.value
-  const row_height = 300
-  var real_i = 0
-  for (const i in processed_models.value) {
-    const row = Math.floor(Number(real_i) / col_num.value)
-    const col = Number(real_i) % col_num.value
-    if (visible_models_id.value.indexOf(processed_models.value[i].id) === -1) {
-      // 移动到正中央
-      pos.value[processed_models.value[i].id] = {
-        x: window.innerWidth / 2,
-        y: 120
-      }
-      widths.value[processed_models.value[i].id] = 0
-      continue
-    }
-    pos.value[processed_models.value[i].id] = {
-      x: padding_left + col * (col_width + gap),
-      y: row * (row_height + gap) + padding_top
-    }
-    widths.value[processed_models.value[i].id] = col_width
-    real_i ++;
-  }
+  calcCardStyle(  )
 
   return calcColNum
 }
+
+
 window.onresize = calcColNum()
 
 const search_val = ref("")
 const search = (val: string) => {
-  console.log(search_val);
+  console.log(search_val.value);
   
   search_val.value = val
+  visible_models_id.value = {}
   if (val === "") {
-    visible_models_id.value = processed_models.value.map(item => item.id)
+    processed_models.value.map(item => visible_models_id.value[item.id] = true)
   } else {
-    visible_models_id.value = processed_models.value.filter(item => item.title.indexOf(val) !== -1 || item.desc.indexOf(val) !== -1).map(item => item.id)
+    processed_models.value.filter(item => item.title.toLowerCase().indexOf(val) !== -1 || item.desc.toLowerCase().indexOf(val) !== -1).map(item => visible_models_id.value[item.id] = true)
   }
   calcColNum()
+  calcCardStyle()
 }
-// @ts-ignore
-window.calcColNum = calcColNum
 watch(search_val, search)
+
+onMounted(() => {
+  init()
+  calcColNum()
+})
 </script>
 
 <style lang="less" scoped>
